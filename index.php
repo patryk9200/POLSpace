ecf5307490a6710eada9b2aa9001de2da6b8b757
Pawel Kuszka <pawelkuszka@gmail.com>
2019-10-17 11:57:24 +0200

    Signed-off-by: Pawel Kuszka <pawelkuszka@gmail.com>

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="theme-color" content="#00254a">
+		<meta charset="utf-8">	
+		
+		<title></title>
+		
+	</head>
+	<body>
+	
+		<div class="leftMenu">
+			
+			<div class="left_menu_content">
+				
+				<div class="logo">
+					POLSpace
+				</div>
+				
+				<ul>
+					<li><a href="default.asp">O nas</a></li>
+					<li><a href="news.asp">Projekty</a></li>
+					<li><a href="contact.asp">Wydarzenia</a></li>
+					<li><a href="about.asp">Kontakt</a></li>
+					<li><a href="about.asp">Logowanie</a></li>
+					<li><a href="about.asp">Język :  PL</a></li>
+					
+				</ul> 
+					
+				
+		
+				
+			</div>
+			
+		</div>
+			
+			
+			
+		<div class="header">
+	
+			
+			<div class="logo">
+				POLSpace
+			</div>
+		
+			<div class="headerMenu">
+				<ul>
+				  <li><a href="default.asp">O nas</a></li>
+				  <li><a href="news.asp">Projekty</a></li>
+				  <li><a href="contact.asp">Wydarzenia</a></li>
+				  <li><a href="about.asp">Kontakt</a></li>
+				</ul> 
+			</div>	
+			
+			<div class="rightMenu">
+				
+				<div class="languagesBar"> <i class="fas fa-globe"></i> PL </div>
+				<div class="loginButton">Logowanie</div>
+				
+			</div>
+			
+			<div class="right_open_menu">
+				<i class="fas fa-bars"></i>
+			</div>
+			
+		</div>				
+						
+		
+		
+		
+		<div class="slider">
+					
+			<div id="clouds"></div>
+
+			
+		</div>
+		
+		<div class="page">
+			
+			<div class="sectionWho">
+			
+				<h1>Kim jesteśmy?</h1>
+				
+				Jesteśmy ponad 40 osobową grupą specjalistów wszelakiej maści w tym - programistów, elektryków, elektroników, mechaników, meteorologow, astronomów, modelarzy RC. Połączyła nas nasze wspólne zainteresowania oraz chęć popularyzacji przemysłu kosmicznego w Polsce.
+				
+			</div>
+			
+			<div class="sectionWhat">
+			
+				<h1>Co robimy?</h1>
+				
+				
+				Na co dzień zajmujemy się swoimi zawodowymi obowiązkami - jednak w czasie wolnym każdy z nas bierze czynny udział w projektach realizowanych przez : "nasza-nazwa". Są to projekty ukierunkowane na rozwój specjalistycznego oprogramowania wykorzystywanego w przestworzach.
+				Prowadzimy rówież wszlekiego rodzaju prace badwczo-rozwowe.
+
+				
+			</div>
+			
+			<div class="sectionJoin">
+			
+				<h1>Dołącz do nas</h1>
+				
+				Projekt "POLSPACE" łączy ze sobą ludzi o różnych charakterach, zawodach ale z jedną pasją do nauki. Jeśli interesuje Cię nasz projekt i chcesz poznać ludzi o podobnych zainteresowaniach dołącz do nas już dziś! 
+				
+			</div>
+			
+			<div class="sectionProjects">
+			
+				<h1>Projekty</h1>
+				
+				<br/>
+				<div class="ProjectFull">
+					<div class="ProjectBox">
+						<img src="https://www.extremetech.com/wp-content/uploads/2013/03/spacex-normandy-640x353.jpg" class="image_spec" >
+						<div class="centered">Coworking</div>
+					</div>
+				</div>
+
+				<div class="ProjectFull">
+					<div class="ProjectBox">
+						<img src="https://www.extremetech.com/wp-content/uploads/2013/03/spacex-normandy-640x353.jpg" class="image_spec" >
+						<div class="centered">Events</div>
+					</div>
+				</div>	
+
+				<div class="ProjectFull">
+					<div class="ProjectBox">
+						<img src="https://www.extremetech.com/wp-content/uploads/2013/03/spacex-normandy-640x353.jpg" class="image_spec" >
+						<div class="centered">Rest</div>
+					</div>
+				</div>			
+				
+				
+			
+			</div>
+			
+			<div class="sectionEvents">
+				
+				<h1>Wydarzenia</h1>
+				
+				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+			
+			</div>
+			
+			<div class="sectionContact">
+			
+				<h1>Kontakt</h1>
+				
+				
+				<div class="fullPanelContact">
+					<div class="LeftContact">
+						
+						<form>
+							<label>Imię</label> <br/>
+							<input type="text">
+							<br/>
+							<label>E-mail</label> <br/>
+							<input type="text">
+							<br/>
+							<label>Wiadomość</label> <br/>
+							<textarea></textarea>
+							<br/><br/>
+							<input type="submit" value="Wyślij Wiadomosć">
+							
+							<br/><br/><br/><br/>
+							
+							<b>POLSpace</b> w mediach <br/><Br/>
+							
+							<i class="fab fa-facebook"></i> <span class="medias">Facebook</span>
+							&nbsp; &nbsp; &nbsp; 
+							<i class="fab fa-youtube"></i> <span class="medias">Youtube</span>
+						</form>
+						
+					</div>
+					<div class="RightContact">
+					
+					</div>
+				</div>
+				
+			</div>
+			
+		</div>
+		
+		<div class="footer">
+			<div class="page">
+			Copyright 2019 by <b>POLSpace</b> Team | Design: <u>Marcel Woszkowski</u> | FrontEnd: <u>Paweł Kuszka</u> | BackEnd: <u>Patryk Golus</u>
+
+			</div>
+		</div>
+	
+		<script src="//cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>	
+		<script>
+			
+			$(".leftMenu").click(function(e) {
+				
+				 
+				 if(e.target !== e.currentTarget) return;
+				 
+				 
+				 $(".leftMenu").hide();
+				
+				
+				 
+				 
+			});	
+			
+			$( ".right_open_menu" ).click(function() {
+				$(".leftMenu").show("slide", { direction: "left" }, 1000);
+			});
+		
+			// window load event makes sure image is // loaded before running animation
+			$(window).on("load", function() {
+
+			var tl = new TimelineMax({repeat:-1});
+			tl.to("#clouds", 30, {
+				backgroundPosition: "-2247px 0px",
+				//autoRound:false,
+				ease: Linear.easeNone
+			});
+
+			});
+			
+			
+			
+			
+			
+			
+		</script>
+		
+			
+	</body>
+</html>	
+	
